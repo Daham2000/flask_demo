268b52c6eb7f14f32214c4b3d334924377fa142e
Daham2000 <dahamakalanka2000@gmail.com>
2021-10-16 11:06:34 +0530

    removed cache file

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-from flask import Flask,render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    print("Hello world")
-    return render_template('index.html',name="Daham")
-
-app.run(debug=True)
+print("Hello world!!!")
